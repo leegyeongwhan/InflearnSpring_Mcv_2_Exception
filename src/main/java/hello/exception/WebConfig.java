31a938cf5b8c87fb214bd5f42005d77f2b2a55e6
leegyeongwhan <leekhy02@naver.com>
2022-11-13 23:39:31 +0900

    feat(mvc2 예외처리): 로그인 인터셉터 과정과 처리

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package hello.exception;
 
 import hello.exception.fillter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -22,5 +24,17 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST,
                 DispatcherType.ERROR);
         return filterRegistrationBean;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/css/**", "/*.ico"
+                        , "/error", "/error-page/**" //오류 페이지 경로
+                );
     }
 }
