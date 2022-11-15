acdd9363748beea87ac73593e4a8727577bcb44e
leegyeongwhan <leekhy02@naver.com>
2022-11-15 12:13:19 +0900

    feat(mvc2 예외처리): 스프링이 제공하는 ExceptionResolver2
    스프링이 제공하는 예외 처리 리졸버 형태를 구현 스프링에서의 예외처리 방식

---
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -0,0 +1,10 @@
+package hello.exception.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ApiExceptionV2Controller {
+
+}
